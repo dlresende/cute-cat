15e54a00f37ad5ab70c0f1f73e5f4469745c1abc
Diego Lemos <diegolemosderesende@gmail.com>
2013-09-29 18:05:20 +0200

    Enrich getCatImg test.

--- test/js/cuteCatTest.js
+++ test/js/cuteCatTest.js
@@ -1,5 +1,7 @@
 test( "given an index, should get the image associated", function() {
     equal(getCatImg(1), "images/cat1.jpg" );
+    equal(getCatImg(2), "images/cat2.jpg" );
+    equal(getCatImg(3), "images/cat3.jpg" );
 });
 
 test( "should calculate the index of the next cat to be shown", function() {
