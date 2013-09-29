0a7593bc49f8b3bb2a3c60a97255f5a41739aa96
Diego Lemos <diegolemosderesende@gmail.com>
2013-09-29 17:48:24 +0200

    Implemented: calculate the index of the next cat to be shown.

--- src/js/cuteCat.js
+++ src/js/cuteCat.js
@@ -1,3 +1,11 @@
 function getCatImg(imgIndex)  {
     return "images/cat" + imgIndex + ".jpg";
 }
+
+function nextCatIndex(firstCat, secondCat, numberOfCats) {
+    do {
+        var imgIndex = Math.floor((Math.random() * numberOfCats) + 1);
+    } while(imgIndex == firstCat || imgIndex == secondCat);
+
+    return imgIndex;
+}
--- test/js/cuteCatTest.js
+++ test/js/cuteCatTest.js
@@ -1,3 +1,11 @@
 test( "given an index, should get the image associated", function() {
     equal(getCatImg(1), "images/cat1.jpg" );
 });
+
+test( "should calculate the index of the next cat to be shown", function() {
+   equal(nextCatIndex(1, 2, 3), 3);
+   equal(nextCatIndex(2, 1, 3), 3);
+   equal(nextCatIndex(null, null, 1), 1);
+   equal(nextCatIndex(null, 1, 2), 2);
+   equal(nextCatIndex(1, null, 2), 2);
+});
